19a7dec57a6111c80e3dd92bbdea9863b9ba321f
ElaKedir <eliaskedir516@gmail.com>
2024-01-04 17:23:50 -0800

    initial commit

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Ela's addition function
+
+    Args:
+        a: the first integer
+        b: the second integer
+
+    Returns:
+        The return value. a + b
+    """
+
+    return (a + b)
+
