a6659166d53fe34cd39b7d2c1a5a3b57a091dd4c
ElaKedir <eliaskedir516@gmail.com>
2024-01-04 17:34:45 -0800

    initial commit

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Ela's addition function
+
+    Arguments:
+        a: the first integer
+        b: the second integer
+
+    Returns:
+        The return value of. a + b
+    """
+
+    return (a + b)
+
+
+def sub(a, b):
+    """Ela's subtraction function
+
+    Arguments:
+        a: the first integer
+        b: the second integer
+
+    Returns:
+        The return value of. a - b
+    """
+
+    return (a - b)
+
+
+def mul(a, b):
+    """Ela's multiplication function
+
+    Arguments:
+        a: the first integer
+        b: the second integer
+
+    Returns:
+        The return value of. a * b
+    """
+
+    return (a * b)
+
+
+def div(a, b):
+    """ela's division function
+
+    Arguments:
+        a: the first integer
+        b: the second integer
+
+    Returns:
+        The return value of. a / b
+    """
+
+    return int(a / b)
+
