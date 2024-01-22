2d3d19c30595fc2280695c72620b9485a2e116a4
ElaKedir <eliaskedir516@gmail.com>
2024-01-23 00:23:56 +0300

    initial commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    """Print x elememts of a list.
+    """A function that Print x elememts of a list.
 
     Args:
-        my_list (list): The list to print elements from.
-        x (int): The number of elements of my_list to print.
+        my_list (list): The list.
+        x (int): The number of elements to be printed.
 
     Returns:
         The number of elements printed.
---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+	""" A function that prints an integer with "{:d}".format().
+
+	Args:
+		value (int): The list.
+		x (int): The number of elements to print from the list.
+
+	Returns:
+		The number of elements printed from the list.
+	"""
