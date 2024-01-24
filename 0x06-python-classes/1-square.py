5e1c8f35104421b9f1cecd13105fae02be4dd313
ElaKedir <eliaskedir516@gmail.com>
2024-01-24 04:14:44 +0300

    initial commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A class Square that defines a square"""
+
+
+class Square:
+    """ A class Square that defines a square"""
+
+    def __init__(self, size):
+        """initialize the square class
+        Args:
+            size (int): the size of the square
+        """
+        self.__size = size  #: size of the square
