2ffd55f19dec222fc3ca7dfb534e3ff5ebb480e7
ElaKedir <eliaskedir516@gmail.com>
2024-01-24 04:56:55 +0300

    initial commit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -52,18 +52,3 @@ class Square:
                 print()
         else:
             print()
-
-my_square = Square(3)
-my_square.my_print()
-
-print("--")
-
-my_square.size = 10
-my_square.my_print()
-
-print("--")
-
-my_square.size = 0
-my_square.my_print()
-
-print("--")
