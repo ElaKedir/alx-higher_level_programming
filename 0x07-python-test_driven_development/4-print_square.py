9ee1a591bc20b7ca8d4dea39c48585ec8807a87f
ElaKedir <eliaskedir516@gmail.com>
2024-01-31 05:48:22 +0300

    changing permission types

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+
+A module composed of functions that prints a square with the character #
+
+"""
+
+
+def print_square(size):
+    """ A function that prints a square with the character #
+
+    Args:
+        size: size of the square printed
+
+    Returns:
+        Nothing
+
+    Raises:
+        TypeError: If size is not an integer number
+
+
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
