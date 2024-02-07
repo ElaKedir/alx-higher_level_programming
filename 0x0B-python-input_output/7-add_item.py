5952dedfd2d1152a13fcf4ef289dfc62d63312ce
ElaKedir <eliaskedir516@gmail.com>
2024-02-06 16:53:47 -0800

    initial commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" A script that Add all arguments to a Python list and save them to a file."""
+""" A script that Add all arguments to a Python list and save them to a
+file."""
 import sys
 
 if __name__ == "__main__":
