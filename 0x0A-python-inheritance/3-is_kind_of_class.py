52f77f90c8fd4f25130cffe6dedf0224647c95fc
ElaKedir <eliaskedir516@gmail.com>
2024-02-07 15:54:40 +0300

    Initial commit

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -8,4 +8,7 @@ def is_same_class(obj, a_class):
     """returns True if the object is an instance of, or if the object is
     an instance of a class that inherited from, the specified class
     otherwise False"""
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
