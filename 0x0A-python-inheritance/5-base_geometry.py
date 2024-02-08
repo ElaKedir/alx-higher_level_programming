dd401dd5ce17c1b1dba276f88cbba26a0584f106
ElaKedir <eliaskedir516@gmail.com>
2024-02-07 18:03:43 -0800

    initial commit

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A module base geometry
+"""
+
+
+class BaseGeometry:
+    """ empty class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A module base geometry
+"""
+
+
+class BaseGeometry:
+    """ empty class"""
+    def area(self):
+        raise Exception('area() is not implemented')
